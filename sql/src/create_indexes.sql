a7f40845683a22833707e8c72b172f98628b9f6d
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-06-07 01:55:40 -0700

    added trigers and idx

---
+++ sql/src/create_indexes.sql
@@ -0,0 +1,11 @@
+DROP INDEX IF EXISTS idx_users_phoneNum;
+DROP INDEX IF EXISTS idx_catalog_genre;
+DROP INDEX IF EXISTS idx_rentalOrder_login;
+DROP INDEX IF EXISTS idx_trackingInfo_rentalOrderID;
+DROP INDEX IF EXISTS idx_gamesInOrder_gameID;
+
+CREATE INDEX idx_users_phoneNum ON Users(phoneNum);
+CREATE INDEX idx_catalog_genre ON Catalog(genre);
+CREATE INDEX idx_rentalOrder_login ON RentalOrder(login);
+CREATE INDEX idx_trackingInfo_rentalOrderID ON TrackingInfo(rentalOrderID);
+CREATE INDEX idx_gamesInOrder_gameID ON GamesInOrder(gameID);
--- sql/src/create_triggers.sql
+++ sql/src/create_triggers.sql
@@ -1,10 +1,40 @@
-CREATE OR REPLACE FUNCTION update_catalog_units()
+CREATE OR REPLACE FUNCTION update_overdue_games() 
 RETURNS TRIGGER AS $$
 BEGIN
-    UPDATE GamesInOrder
-    SET unitsOrdered = unitsOrdered - NEW.unitsordered 
-    WHERE gameID AND gameName = NEW.gameName;
+    IF NEW.dueDate < NOW() THEN
+        UPDATE Users SET numOverDueGames = numOverDueGames + 1 WHERE login = NEW.login;
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
+
+CREATE TRIGGER check_overdue_games
+AFTER INSERT OR UPDATE ON RentalOrder
+FOR EACH ROW
+EXECUTE PROCEDURE update_overdue_games();
+
+
+
+CREATE OR REPLACE FUNCTION update_total_price()
+RETURNS TRIGGER AS $$
+DECLARE
+    game_price DECIMAL(10,2);
+    total_price DECIMAL(10,2) := 0;
+BEGIN
+    FOR game_price IN
+        SELECT price * unitsOrdered
+        FROM GamesInOrder JOIN Catalog ON GamesInOrder.gameID = Catalog.gameID
+        WHERE rentalOrderID = NEW.rentalOrderID
+    LOOP
+        total_price := total_price + game_price;
+    END LOOP;
+    UPDATE RentalOrder SET totalPrice = total_price WHERE rentalOrderID = NEW.rentalOrderID;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER recalculate_total_price
+AFTER INSERT OR UPDATE ON GamesInOrder
+FOR EACH ROW
+EXECUTE PROCEDURE update_total_price();
